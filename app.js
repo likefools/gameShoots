0b5f1801f644abbbbe6dd62557c51833faa06737
israel mailman <80362556+likefools@users.noreply.github.com>
2021-11-24 23:34:12 +0200

    שינוי קטן

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 
-var numOfTrdet = 10;
+var numOfItem = 10;
 var myTimer;
 var start = false;
-
+const memo = []
+// מריץ מספר רוביעים ומכניס להם תמונה מוסתרת
 function mytarget(){
-    for (var i=0;i <numOfTrdet; i++) {
-        let gridItem = $("<div class=\"grid-item "+i+"\"></div>");
+    // לאוסיף אנימציה של שניה לכל ריבוע
+    for (let i=0;i <numOfItem; i++) {
+        
+        let gridItem = $("<div class=\"grid-item target"+i+"\"></div>");
         $(".grid-container").append(gridItem);
         let target = '<img src="imges/targetl.png" alt="targetl"></img>';
-        $('.'+i).prepend(target);
-        $('.'+i+ ' img').hide();
+        
+        $('.target'+i).prepend(target);
+        memo[i] =$('.target'+i+ ' img').hide();
     }
+    console.log(memo);
     // let j = Math.floor(Math.random() * 10);
     // let target = '<img src="imges/targetl.png" alt="targetl"></img>';
     //     $('.'+j).prepend(target);
 }
-
 mytarget();
+
 
 // class name '1' = img
 $('.start').click(function(){
@@ -37,22 +42,38 @@ $('.stop').click(function(){
 // const mySet = new Set();
 
 
+
+
+
+
+
+
+
 let i = 0;
 function myTimer(){
-    let windowTrdet = Math.floor(Math.random() * numOfTrdet);
-    console.log(windowTrdet);
+    // מקבל מספר רנדומלי
+    let randomItem = Math.floor(Math.random() * numOfItem);
+    console.log(memo[randomItem]);
+    // בודק אם הפריט כבר קיים
+    if ($('.target'+randomItem+ ' img').is(':visible') != true){
+        // console.log(randomItem);
+        // $('.target'+randomItem+ ' img').show();
+        $(memo[randomItem]).show();
+        // setTimeout(hideImg, 3000);
+    }
+
     // console.log(mySet);
-    // if (!mySet.has(windowTrdet)){
-        // mySet.add(windowTrdet);
-        $('.'+windowTrdet+ ' img').show();
-        // mySet.delete(windowTrdet);
-        setTimeout(hideImg, 3000);
+    // if (!mySet.has(randomItem)){
+        // mySet.add(randomItem);
+        // $('.target'+randomItem+ ' img').show();
+        // mySet.delete(randomItem);
+        // setTimeout(hideImg, 3000);
         
     // }
 
     function hideImg(){
-        if ($('.'+windowTrdet+ ' img').is(':visible') == true) { // '.1 > img'
-            $('.'+windowTrdet+ ' img').hide();
+        if ($('.target'+randomItem+ ' img').is(':visible') == true) { // '.1 > img'
+            $('.target'+randomItem+ ' img').hide();
         }
     }
     
@@ -68,8 +89,8 @@ function myTimer(){
         
     });
     i++;
-    $('h3').text("i is " + i);
-    if (i == 5){
+    $('h3 span').text(i);
+    if (i == 20){
         myStopFunction();
     }
     
