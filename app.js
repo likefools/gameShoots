c779041ee1ce6115c0691e41b24d41f3be99b954
likefools <yisraelk1@walla.com>
2021-11-25 14:34:17 +0200

    add key

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ var numOfItem = 10;
 var myTimer;
 var start = false;
 
+const memo = new Map([]);
 // מריץ מספר רוביעים ומכניס להם תמונה מוסתרת
 function mytarget(){
     // לאוסיף אנימציה של שניה לכל ריבוע
     for (let i=0;i <numOfItem; i++) {
+        
         let gridItem = $("<div class=\"grid-item target"+i+"\"></div>");
         $(".grid-container").append(gridItem);
         let target = '<img src="imges/targetl.png" alt="targetl"></img>';
+        
         $('.target'+i).prepend(target);
-        $('.target'+i+ ' img').hide();
+        // memo[i] =$('.target'+i+ ' img').hide();
+        memo.set(i, $('.target'+i+ ' img').hide());
     }
+    console.log(memo.size);
     // let j = Math.floor(Math.random() * 10);
     // let target = '<img src="imges/targetl.png" alt="targetl"></img>';
     //     $('.'+j).prepend(target);
@@ -38,16 +43,30 @@ $('.stop').click(function(){
 
 // const mySet = new Set();
 
+for (let role of memo.values()) {
+    console.log(role);
+}
+
+// if (memo.values == $('.target'+random+ ' img').show()){
+//     console.log("show");
+// }
+for ( i=0; i > 10; )
+const random = Math.floor(Math.random() * memo.size);
+
+memo.set(random, $('.target'+random+ ' img').show());
+
 
 let i = 0;
 function myTimer(){
     // מקבל מספר רנדומלי
-    let randomItem = Math.floor(Math.random() * numOfItem);
+    let randomItem = Math.floor(Math.random() * memo.length);
+    console.log(randomItem, memo[randomItem]);
     // בודק אם הפריט כבר קיים
     if ($('.target'+randomItem+ ' img').is(':visible') != true){
-        console.log(randomItem);
-        $('.target'+randomItem+ ' img').show();
-        setTimeout(hideImg, 3000);
+        // console.log(randomItem);
+        // $('.target'+randomItem+ ' img').show();
+        $(memo[randomItem]).show();
+        // setTimeout(hideImg, 3000);
     }
 
     // console.log(mySet);
